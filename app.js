266e7909fce6f815064394712599d6aa5ddb9b1d
vaishanviparabkar90 <parabkarvaishnavi24@gmail.com>
2025-12-26 13:50:33 +0530

    some small changes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 app.use(express.static("public")); // serve index.html
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
 });
 
 export default app;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import http from "http";
 import app from "./app.js";
 import { initSocket } from "./Socket/SocketInit.js";
@@ -5,7 +7,7 @@ import { initSocket } from "./Socket/SocketInit.js";
 const server = http.createServer(app);
 initSocket(server);
 
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
